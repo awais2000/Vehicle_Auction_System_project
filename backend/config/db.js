65c743406acb7b2d14f3fbaa9263a95949f47ba9
awais2000 <135193628+awais2000@users.noreply.github.com>
2025-08-02 00:16:21 -0700

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: process.env.DB_HOST || "localhost",
-//   user: process.env.DB_USER || "root",
-//   password: process.env.DB_PASSWORD || "root",
-//   database: process.env.DB_NAME || "copart",
-// });
-
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
